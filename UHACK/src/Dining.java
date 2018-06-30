3f371809375b4974f36b9153cee163358b98154f
mbaarde <40690319+mbaarde@users.noreply.github.com>
2018-06-30 09:04:39 +0800

    added dining entertainment and shopping

---
+++ UHACK/src/Dining.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Dining extends Category
+{
+    public Dining(String name, String mall, int queuelength){
+        super(name, mall, queuelength);
+    }
+    
+}
+
---
+++ UHACK/src/Entertainment.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Entertainment extends Category
+{
+    public Entertainment(String name, String mall, int queuelength){
+        super(name, mall, queuelength);
+    }
+    
+}
+
