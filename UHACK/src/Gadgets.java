bbe901c57027fa655e360ce7770f9249e5f5605a
Joey Sayson <jose_vicente_sayson@dlsu.edu.ph>
2018-06-30 09:17:36 -0700

    New classes
    New classes for gadgets and services categories

---
+++ UHACK/src/Gadgets.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Belle
+ */
+public class Gadgets extends Category{
+    
+    public Gadgets(String name, String mall, int queuelength) {
+        super(name, mall, queuelength);
+    }
+    
+    
+}
